9a26887893677627a8ea4da3a9246572e12ecd53
codbuilder <77922829+codbuilder@users.noreply.github.com>
2021-04-06 14:55:18 +0300

    continued-2

--- app.js
+++ app.js
@@ -17,62 +17,70 @@ const getRep = async repositoriesUrl => {
 		throw new Error('Failed to fetch user')
 	}
 }
+
 const infoblock = document.querySelector('.infoblock')
 const itemsList = document.querySelector('.infoblock__items')
 
-getUser('facebook')
-	.then(userData => {
-		infoblock.insertAdjacentHTML(
-			'afterbegin',
-			`<img class="infoblock__avatar" src="${userData['avatar_url']}">`
-		)
-		itemsList.insertAdjacentHTML(
-			'beforeend',
-			`<li><a href="${userData['html_url']}" target="_blank">${userData['login']}</a></li>`
-		)
+const formButton = document.querySelector('.findform__submit')
 
-		switch (true) {
-			case !!userData['location']:
-				itemsList.insertAdjacentHTML(
-					'beforeend',
-					`<li class="items__location">${userData['location']}</li>`
-				)
-			case !!userData['bio']:
-				itemsList.insertAdjacentHTML('beforeend', `<li>${userData['bio']}</li>`)
-		}
+formButton.addEventListener('click', () => {
+	let formInputValue = document.querySelector('.findform__input').value
+	console.log(formInputValue)
+})
 
-		itemsList.insertAdjacentHTML(
-			'beforeend',
-			`<li><strong>Repositories (${userData['public_repos']}):</strong></li>`
-		)
-		itemsList.insertAdjacentHTML('beforeend', `<li class="items__rep"></li>`)
-		let itemsRep = document.querySelector('.infoblock__avatar')
+// getUser('facebook')
+// 	.then(userData => {
+// 		infoblock.insertAdjacentHTML(
+// 			'afterbegin',
+// 			`<img class="infoblock__avatar" src="${userData['avatar_url']}">`
+// 		)
+// 		itemsList.insertAdjacentHTML(
+// 			'beforeend',
+// 			`<li><a href="${userData['html_url']}" target="_blank">${userData['login']}</a></li>`
+// 		)
 
-		console.log(userData.repos_url)
-		let repsUrl = ''
-		getRep(repsUrl).then(repUrl => {
-			console.log(repUrl[18])
-		})
-		// const keys = Object.keys(userData)
-		// keys.forEach(key => {
-		// 	// console.log(`${key} : ${userData[key]}`)
-		// 	// let newNode = document.createElement('li')
-		// 	// newNode.className = 'infoblock__item'
-		// 	// newNode.innerHTML = `${key} : ${userData[key]}`
-		// 	// itemsList.append(newNode)
-		// 	let k, kValue
-		// 	switch (key) {
-		// 		case 'avatar_url':
-		// 			itemAvatar.setAttribute('src', `${userData[key]}`)
-		// 			k = key
-		// 			kValue = userData[key]
-		// 			break
-		// 		default:
-		// 			k = key
-		// 			kValue = userData[key]
-		// 	}
-		// 	let html = `<li class="infoblock__item"><span>${k}</span><span>${kValue}</span></li>`
-		// 	itemsList.insertAdjacentHTML('beforeend', html)
-		// })
-	})
-	.catch(err => alert(err.message))
+// 		switch (true) {
+// 			case !!userData['location']:
+// 				itemsList.insertAdjacentHTML(
+// 					'beforeend',
+// 					`<li class="items__location">${userData['location']}</li>`
+// 				)
+// 			case !!userData['bio']:
+// 				itemsList.insertAdjacentHTML('beforeend', `<li>${userData['bio']}</li>`)
+// 		}
+
+// 		itemsList.insertAdjacentHTML(
+// 			'beforeend',
+// 			`<li><strong>Repositories (${userData['public_repos']}):</strong></li>`
+// 		)
+// 		itemsList.insertAdjacentHTML('beforeend', `<li class="items__rep"></li>`)
+// 		let itemsRep = document.querySelector('.infoblock__avatar')
+
+// 		console.log(userData.repos_url)
+// 		// let repsUrl = ''
+// 		// getRep(repsUrl).then(repUrl => {
+// 		// 	console.log(repUrl[18])
+// 		// })
+// 		// const keys = Object.keys(userData)
+// 		// keys.forEach(key => {
+// 		// 	// console.log(`${key} : ${userData[key]}`)
+// 		// 	// let newNode = document.createElement('li')
+// 		// 	// newNode.className = 'infoblock__item'
+// 		// 	// newNode.innerHTML = `${key} : ${userData[key]}`
+// 		// 	// itemsList.append(newNode)
+// 		// 	let k, kValue
+// 		// 	switch (key) {
+// 		// 		case 'avatar_url':
+// 		// 			itemAvatar.setAttribute('src', `${userData[key]}`)
+// 		// 			k = key
+// 		// 			kValue = userData[key]
+// 		// 			break
+// 		// 		default:
+// 		// 			k = key
+// 		// 			kValue = userData[key]
+// 		// 	}
+// 		// 	let html = `<li class="infoblock__item"><span>${k}</span><span>${kValue}</span></li>`
+// 		// 	itemsList.insertAdjacentHTML('beforeend', html)
+// 		// })
+// 	})
+// 	.catch(err => alert(err.message))
