f0d4d9420c8ca1b5e68457c212c3b360dee913c1
codbuilder <77922829+codbuilder@users.noreply.github.com>
2021-04-07 23:17:25 +0300

    firstFinal

--- app.js
+++ app.js
@@ -15,8 +15,10 @@ const getUser = async userId => {
 
 const getRep = async repositoriesUrl => {
 	try {
+		loader.classList.remove('loader_hide')
 		const response = await fetch(repositoriesUrl)
 		const repData = await response.json()
+		loader.classList.add('loader_hide')
 		return repData
 	} catch (err) {
 		throw new Error('Failed to fetch user')
@@ -34,22 +36,10 @@ formInput.addEventListener('input', e => {
 	formInput.value = formInput.value.replace(/\s+/gi, '')
 	if (formInput.value.length) {
 		formButton.removeAttribute('disabled')
-		//console.log(formInput.value, '-', formInput.value.length)
 	} else {
 		formButton.setAttribute('disabled', 'disabled')
 	}
 })
-
-const objKeys = [
-	// 'name',
-	// 'html_url',
-	'location',
-	'bio',
-	'public_repos',
-	'repos_url',
-	'followers',
-	'following',
-]
 
 formButton.addEventListener('click', () => {
 	let formInputValue = formInput.value
@@ -61,40 +51,67 @@ formButton.addEventListener('click', () => {
 		.then(userData => {
 			itemsList.innerText = ''
 			avatar.setAttribute('src', userData.avatar_url)
-			console.log(userData.avatar_url)
+			console.log(userData)
 			if (userData.name) {
 				itemsList.insertAdjacentHTML(
 					'beforeend',
-					`<li><a href="${userData.html_url}">${userData.name}</a></li>`
+					`<li><a href="${userData.html_url}" target="_blank">${userData.name}</a> — @${userData.login}</li>`
 				)
 			} else {
 				itemsList.insertAdjacentHTML(
 					'beforeend',
-					`<li><a href="${userData.html_url}">${userData.login}</a></li>`
+					`<li><a href="${userData.html_url}">@${userData.login}</a></li>`
 				)
 			}
-
+			// location
 			if (userData.location) {
 				itemsList.insertAdjacentHTML(
 					'beforeend',
 					`<li class="items__location">${userData.location}</li>`
 				)
 			}
-			Object.keys(userData).forEach(key => {
-				let addClass = `class="items__${key}"`
-				switch (key) {
-					case 'name':
-						itemsList.insertAdjacentHTML(
-							'beforeend',
-							`<li ${addClass}>${key}: ${userData[key]}</li>`
-						)
-					default:
-						itemsList.insertAdjacentHTML(
-							'beforeend',
-							`<li ${addClass}>${key}: ${userData[key]}</li>`
-						)
-				}
-			})
+			// bio
+			if (userData.bio) {
+				itemsList.insertAdjacentHTML(
+					'beforeend',
+					`<li class="items__bio">${userData.bio}</li>`
+				)
+			}
+			// public_repos
+			if (userData.public_repos) {
+				itemsList.insertAdjacentHTML(
+					'beforeend',
+					`<li class="items__repos"><strong>Public repositories: ${userData.public_repos}</strong></li>`
+				)
+			}
+			// followers, following
+			if (userData.followers || userData.following) {
+				itemsList.insertAdjacentHTML(
+					'beforeend',
+					`<li class="items__follow">
+						<p>Followers: ${userData.followers}</p> ・ <p>Following: ${userData.following}</p>
+					</li>`
+				)
+			}
+			// repositories
+			if (userData.repos_url) {
+				itemsList.insertAdjacentHTML(
+					'beforeend',
+					`<li class="items__repos-url"></li>`
+				)
+				getRep(userData.repos_url)
+					.then(repos => {
+						console.log(typeof repos, repos)
+						let repListBlock = document.querySelector('.items__repos-url')
+						Object.keys(repos).forEach(function(key) {
+							repListBlock.insertAdjacentHTML(
+								'beforeend',
+								`<a href="${this[key].html_url}" target="_blank">${this[key].name}</a>`
+							)
+						}, repos)
+					})
+					.catch(err => alert(err.message))
+			}
 		})
 		.catch(err => alert(err.message))
 })
