593ef2e1bb355f68b85ac86f045513b83b5e0e0b
codbuilder <77922829+codbuilder@users.noreply.github.com>
2021-04-07 11:41:51 +0300

    update

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
+const loader = document.querySelector('.loader')
+
 const getUser = async userId => {
 	try {
+		loader.classList.remove('loader_hide')
 		const response = await fetch(`https://api.github.com/users/${userId}`)
+		console.log(response.ok)
+
 		const userData = await response.json()
+		loader.classList.add('loader_hide')
 		return userData
 	} catch (err) {
-		throw new Error('Failed to fetch user')
+		// throw new Error('Failed to fetch user')
+		console.log('some error')
 	}
 }
 
@@ -19,68 +26,87 @@ const getRep = async repositoriesUrl => {
 }
 
 const infoblock = document.querySelector('.infoblock')
+const avatar = document.querySelector('.infoblock__avatar')
 const itemsList = document.querySelector('.infoblock__items')
 
+const formInput = document.querySelector('.findform__input')
 const formButton = document.querySelector('.findform__submit')
 
-formButton.addEventListener('click', () => {
-	let formInputValue = document.querySelector('.findform__input').value
-	console.log(formInputValue)
+formInput.addEventListener('input', e => {
+	formInput.value = formInput.value.replace(/\s+/gi, '')
+	if (formInput.value.length) {
+		formButton.removeAttribute('disabled')
+		//console.log(formInput.value, '-', formInput.value.length)
+	} else {
+		formButton.setAttribute('disabled', 'disabled')
+	}
 })
 
-// getUser('facebook')
-// 	.then(userData => {
-// 		infoblock.insertAdjacentHTML(
-// 			'afterbegin',
-// 			`<img class="infoblock__avatar" src="${userData['avatar_url']}">`
-// 		)
-// 		itemsList.insertAdjacentHTML(
-// 			'beforeend',
-// 			`<li><a href="${userData['html_url']}" target="_blank">${userData['login']}</a></li>`
-// 		)
+const objKeys = [
+	// 'name',
+	// 'html_url',
+	'location',
+	'bio',
+	'public_repos',
+	'repos_url',
+	'followers',
+	'following',
+]
 
-// 		switch (true) {
-// 			case !!userData['location']:
-// 				itemsList.insertAdjacentHTML(
-// 					'beforeend',
-// 					`<li class="items__location">${userData['location']}</li>`
-// 				)
-// 			case !!userData['bio']:
-// 				itemsList.insertAdjacentHTML('beforeend', `<li>${userData['bio']}</li>`)
-// 		}
+formButton.addEventListener('click', () => {
+	let formInputValue = formInput.value
+	formInput.value = ''
+	formButton.setAttribute('disabled', 'disabled')
+	getUser(formInputValue)
+		.then(userData => {
+			itemsList.innerText = ''
+			avatar.setAttribute('src', userData.avatar_url)
+			if (userData.name) {
+				itemsList.insertAdjacentHTML(
+					'beforeend',
+					`<li><a href="${userData.html_url}">${userData.name}</a></li>`
+				)
+			} else {
+				itemsList.insertAdjacentHTML(
+					'beforeend',
+					`<li><a href="${userData.html_url}">${userData.login}</a></li>`
+				)
+			}
 
-// 		itemsList.insertAdjacentHTML(
-// 			'beforeend',
-// 			`<li><strong>Repositories (${userData['public_repos']}):</strong></li>`
-// 		)
-// 		itemsList.insertAdjacentHTML('beforeend', `<li class="items__rep"></li>`)
-// 		let itemsRep = document.querySelector('.infoblock__avatar')
+			if (userData.location) {
+				itemsList.insertAdjacentHTML(
+					'beforeend',
+					`<li class="items__location">${userData.location}</li>`
+				)
+			}
+			Object.keys(userData).forEach(key => {
+				// console.log(key, ':', userData[key])
+				let addClass = `class="items__${key}"`
+				// itemsList.insertAdjacentHTML(
+				// 	'beforeend',
+				// 	`<li ${addClass}>${key}: ${userData[key]}</li>`
+				// )
+				//
+				switch (key) {
+					case 'name':
+						itemsList.insertAdjacentHTML(
+							'beforeend',
+							`<li ${addClass}>${key}: ${userData[key]}</li>`
+						)
+					default:
+						itemsList.insertAdjacentHTML(
+							'beforeend',
+							`<li ${addClass}>${key}: ${userData[key]}</li>`
+						)
+				}
+			})
 
-// 		console.log(userData.repos_url)
-// 		// let repsUrl = ''
-// 		// getRep(repsUrl).then(repUrl => {
-// 		// 	console.log(repUrl[18])
-// 		// })
-// 		// const keys = Object.keys(userData)
-// 		// keys.forEach(key => {
-// 		// 	// console.log(`${key} : ${userData[key]}`)
-// 		// 	// let newNode = document.createElement('li')
-// 		// 	// newNode.className = 'infoblock__item'
-// 		// 	// newNode.innerHTML = `${key} : ${userData[key]}`
-// 		// 	// itemsList.append(newNode)
-// 		// 	let k, kValue
-// 		// 	switch (key) {
-// 		// 		case 'avatar_url':
-// 		// 			itemAvatar.setAttribute('src', `${userData[key]}`)
-// 		// 			k = key
-// 		// 			kValue = userData[key]
-// 		// 			break
-// 		// 		default:
-// 		// 			k = key
-// 		// 			kValue = userData[key]
-// 		// 	}
-// 		// 	let html = `<li class="infoblock__item"><span>${k}</span><span>${kValue}</span></li>`
-// 		// 	itemsList.insertAdjacentHTML('beforeend', html)
-// 		// })
-// 	})
-// 	.catch(err => alert(err.message))
+			// name, url
+			// document.querySelector('.items__html_url').innerHTML = userData.name
+			// document
+			// 	.querySelector('.items__html_url')
+			// 	.setAttribute('href', userData.html_url)
+			// document.querySelector('.items__html_url').classList.add('.items_show')
+		})
+		.catch(err => alert(err.message))
+})
